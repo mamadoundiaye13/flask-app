c917b1194605bc73ab05ba43e7d4c730292dcf72
Mamadou NDIAYE <mamadoundiaye094@gmail.com>
2020-10-20 15:32:43 +0200

    form

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 from flask import Flask
 from flask import render_template
-
+import requests
 app = Flask(__name__)
 
 """ formulaire """
 @app.route('/')
 def index():
-     return 'doit retourner le formulaire'
+     return render_template('index.html')
 
 @app.route('/repos/')
-def getAllRepos(name=None):
-    return render_template('all-repos.html')
+def getAllRepos():
+    url = "https://api.github.com/users/alex/repos"
+    r = requests.get(url)
+    json_obj = r.json()
+
+    owner = list(json_obj["owner"])
+
+    return render_template('all-repos.html', owner = owner)
+
+@app.route('/user/<name>')
+@app.route('/user/')
+def getUser(name=None):
+    url = "https://api.github.com/users/"+ name
+    r = requests.get(url)
+    user = r.json()
+
+    return render_template('user.html', user = user)
 
 @app.route('/repos/<name>')
 def getOneRepos(name=None):
